510e3d4f052c5d9cfdb179e079be7ccbe0f3fa16
TurtleZhong <xinliangzhong@foxmail.com>
2016-11-24 15:49:06 +0800

    add the aruco example in trackMarker.cpp

--- src/trackMarker.cpp
+++ src/trackMarker.cpp
@@ -10,13 +10,65 @@ using namespace aruco;
 
 int main(int argc, char *argv[])
 {
-    Mat image = imread("/home/m/zxl-opencv/src/lena.png");
-    imshow("image",image);
+
+    /*open the camera*/
+    VideoCapture cap(1);
+    if(!cap.isOpened())
+    {
+        return -1;
+    }
+    Mat InImage;
+    bool stop = false;
+    /*prepare for the camera parameters*/
+
+    CameraParameters camParam;
+    double camMatData[3][3] = { {827.5677869352844, 0, 343.4033028034174},
+                                {0, 827.4226963772862, 232.8485134241352},
+                                {0, 0, 1}
+
+                              };
+    double tmpDistCoeffs[5] = {-0.01245509383293474, 0.050954504930418, -0.003378707118209004, 0.006430462217125062, 0};
+
+    Mat cameraMat = Mat(3, 3, CV_64F, camMatData);
+    Mat diff      = Mat(1, 5, CV_64F, tmpDistCoeffs);
+    Size imgSize(640,480);
+    camParam.setParams(cameraMat, diff, imgSize);
+
+    /*random color*/
+    RNG rng;
+
+    while(!stop)
+    {
+
+        cap >> InImage;
+        MarkerDetector MDetector;
+        vector<Marker> Markers;
+        //detect the marker
+        MDetector.detect(InImage, Markers, camParam, 0.041);
+
+        //draw some info for each marker
+
+        for(int i = 0; i < Markers.size(); i++)
+        {
+            Markers[i].draw(InImage, Scalar(0,0,255), 2);
+        }
+
+        // draw a 3d cube in each marker if there is 3d info
+        if (camParam.isValid() && 410 != -1)
+            for (unsigned int i = 0; i < Markers.size(); i++)
+            {
+                CvDrawingUtils::draw3dCube(InImage, Markers[i], camParam);
+                CvDrawingUtils::draw3dAxis(InImage, Markers[i], camParam);
+            }
 
 
+        imshow("Marker_Tracker", InImage);
 
+        if(waitKey(30) >= 0)
+        {
+            stop = true;
+        }
+    }
 
-
-    waitKey(0);
     return 0;
 }
